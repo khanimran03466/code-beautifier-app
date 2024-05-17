656004c2e744974c0ff72c95eac1eed8d7575e62
imran khan <imran.ayub.khan@accenture.com>
2024-05-18 01:55:45 +0530

    HTML minifier fixed

--- src/components/button-comp/HtmlMinifyBtn.jsx
+++ src/components/button-comp/HtmlMinifyBtn.jsx
@@ -2,15 +2,22 @@ import { useContext } from "react";
 import { myContext } from "../../store/StoreComponent";
 
 const HtmlMinifyBtn = () => {
+  if (typeof process === "undefined") {
+    var process = {
+      env: {
+        NODE_ENV: "production" // You can adjust this value based on your needs
+      }
+    };
+  }
   const { input, setOutput } = useContext(myContext);
 
   const handlerHtmlMinify = () => {
     const minified = input
       // .replace(/<!--[\s\S]*?-->/g, "")
-      // Remove CSS comments
+      // Remove CSS comments-
       .replace(/\/\*[\s\S]*?\*\//g, "")
       // Remove whitespace between tags
-    //   .replace(/>\s+</g, "><")
+      .replace(/>\s+</g, "><")
       // Replace multiple spaces with single space
       .replace(/\s+/g, " ")
       // Minify CSS code within <style> tags while preserving <style> tags
@@ -18,6 +25,8 @@ const HtmlMinifyBtn = () => {
         return match.replace(p1, p1.replace(/\s+/g, " "));
       })
       .trim();
+
+    
 
     setOutput(minified);
   };
